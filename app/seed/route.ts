ff06d2d317a0dbcad223f78db2299699674f2440
juancespedesf <juancespedesf.1994@gmail.com>
2025-01-09 08:35:00 -0500

    upload seed file

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,4 @@
- import bcryptjs from 'bcrypt';
+ import bcrypt from 'bcrypt';
  import { db } from '@vercel/postgres';
  import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
@@ -17,7 +17,7 @@
 
    const insertedUsers = await Promise.all(
      users.map(async (user) => {
-       const hashedPassword = await bcryptjs.hash(user.password, 10);
+       const hashedPassword = await bcrypt.hash(user.password, 10);
        return client.sql`
          INSERT INTO users (id, name, email, password)
          VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
@@ -102,21 +102,17 @@
  }
 
  export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  //   await client.sql`BEGIN`;
-  //   await seedUsers();
-  //   await seedCustomers();
-  //   await seedInvoices();
-  //   await seedRevenue();
-  //   await client.sql`COMMIT`;
+   try {
+     await client.sql`BEGIN`;
+     await seedUsers();
+     await seedCustomers();
+     await seedInvoices();
+     await seedRevenue();
+     await client.sql`COMMIT`;
 
-  //   return Response.json({ message: 'Database seeded successfully' });
-  // } catch (error) {
-  //   await client.sql`ROLLBACK`;
-  //   return Response.json({ error }, { status: 500 });
-  // }
+     return Response.json({ message: 'Database seeded successfully' });
+   } catch (error) {
+     await client.sql`ROLLBACK`;
+     return Response.json({ error }, { status: 500 });
+   }
 }
